33f4fb48e86ee3763741eff22daf3f0622e826d3
Serhii Ivashchenko <Ivashchenkoserezha25@gmail.com>
2025-01-28 19:38:37 +0100

    done work

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -3,13 +3,19 @@ import Contact from "../Contact/Contact";
 import s from "../ContactList/ContactList.module.css";
 import { selectFilteredContacts } from "../../redux/selectors";
 
-const ContactList = () => {
+const ContactList = ({ onDelete }) => {
   const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={s.contactList}>
       {filteredContacts.map(({ id, name, number }) => (
-        <Contact key={id} id={id} name={name} number={number} />
+        <Contact
+          key={id}
+          id={id}
+          name={name}
+          number={number}
+          onDelete={onDelete}
+        />
       ))}
     </ul>
   );
